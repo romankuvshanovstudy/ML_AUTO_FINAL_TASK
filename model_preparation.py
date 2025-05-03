85d44f3115924dd45945c2a8dd200a128c4b69ea
Roman Kuvshanov <kuvschanov.roman@yandex.com>
2025-05-03 10:46:31 +0500

    Added doctrings for data_creation.py and model_preparation.py

--- model_preparation.py
+++ model_preparation.py
@@ -3,6 +3,15 @@ from sklearn.linear_model import LinearRegression
 import joblib
 
 def train_model():
+    """
+    Trains a linear regression model to predict temperature based on the day.
+
+    The function reads preprocessed and scaled training data from 
+    'train/temperature_train_scaled.csv', where it uses the 'Day' column 
+    as the input feature and the 'Temperature' column as the target variable.
+    It fits a LinearRegression model from scikit-learn and saves the trained 
+    model to a file named 'temperature_model.pkl' using joblib for later use.
+    """
     train_data = pd.read_csv("train/temperature_train_scaled.csv")
 
     X_train = train_data[['Day']]
