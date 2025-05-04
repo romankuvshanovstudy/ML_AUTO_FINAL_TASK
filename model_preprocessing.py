b846c73e09ed2e8c4e7503cd366bb12f123f5eb5
DAkkerman <dabrovidakkerman@gmail.com>
2025-05-04 14:41:31 +0500

    Добавил docstrings

--- model_preprocessing.py
+++ model_preprocessing.py
@@ -3,6 +3,20 @@ from sklearn.preprocessing import StandardScaler
 import os
 
 def preprocess_data():
+    """
+    Выполняет предварительную обработку данных о температуре с использованием стандартизации и сохраняет масштабированные данные.
+
+    Читает обучающие и тестовые данные из CSV-файлов, применяет StandardScaler к столбцу 'Temperature'
+    и сохраняет масштабированные данные в новые CSV-файлы.
+
+    Входные файлы:
+        - train/temperature_train.csv: Обучающие данные со столбцом 'Temperature'.
+        - test/temperature_test.csv: Тестовые данные со столбцом 'Temperature'.
+
+    Выходные файлы:
+        - train/temperature_train_scaled.csv: Масштабированные обучающие данные.
+        - test/temperature_test_scaled.csv: Масштабированные тестовые данные.
+    """
     train_data = pd.read_csv("train/temperature_train.csv")
     test_data = pd.read_csv("test/temperature_test.csv")
 
